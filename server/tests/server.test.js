4eaa1ed234462f766809935d5c0ecbfd080a1f2b
Mohamed <mohamedsaadgadalhaq@gmail.com>
2019-02-28 11:20:48 +0200

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,3 +98,36 @@ describe('GET /todos/:id', () =>{
     });
 
 });
+
+describe('DELETE /todos/:id', () =>{
+    it('should remove a todo', (done) => {
+       var hexid = todos[1]._id.toHexString();
+       request(app)
+       .delete(`/todos/${hexid}`)
+       .expect(200)
+       .expect((res) => {
+           expect(res.body.todo._id).toBe(hexid);
+       }).end((err,res) => {
+           if(err) { return done(err); }
+
+        Todo.findById(hexid).then((todo) =>{
+            expect(todo).toNotExist;
+            done();
+        }).catch((e) => done(e));
+       });
+    });
+
+    it('should return 404 if todo not found',(done) =>{
+        request(app)
+        .delete(`/todos/${(new ObjectID()).toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+});
