6cb0d945740747b5dfa7ce97079643d3624a9a15
Mohamed <mohamedsaadgadalhaq@gmail.com>
2019-03-01 04:33:23 +0200

    add config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI ,{ useNewUrlParser: true });
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ app.patch('/todos/:id', (req, res) => {
      body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo)=>{
+  Todo.findOneAndUpdate(id, {$set: body},{new:true}).then((todo)=>{
     if(!todo) {
       return res.status(404).send();
     }
