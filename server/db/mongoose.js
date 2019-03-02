6b15ad32d9e9e796128d2369136d5988cd76e6b8
Mohamed <mohamedsaadgadalhaq@gmail.com>
2019-03-02 03:35:04 +0200

    Add GET /users/me and authentication middleware

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI ,{ useNewUrlParser: true });
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {authenticate} = require('./middleware/authenticate');
 var app = express();
 const port = process.env.PORT ;
 app.use(bodyParser.json());
@@ -89,8 +89,27 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  
 
-app.listen(port, () =>{
+  user.save().then(() => {
+    
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) =>{ res.status(400).send(e) });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+ res.send(req.user);
+});
+
+
+app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
